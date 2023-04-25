fb79a9f888b66f51ff350b88498888da6d33d940
Hrayr94 <hrayrashchyan94@gmail.com>
2023-04-25 16:17:50 +0400

    Axios

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Header from "./components/Header";
+import Users from "./components/Users";
+import AddUser from "./components/AddUser";
+import axios from "axios";
+
+const baseUrl = "https://reqres.in/api/users?page=2";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    axios.get(baseUrl).then((res) => {
+      this.setState({ users: res.data.data });
+    });
+
+    this.state = {
+      users: [],
+    };
+    this.addUser = this.addUser.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
+    this.editUser = this.editUser.bind(this);
+  }
+  render() {
+    return (
+      <div>
+        <Header title="List of users" />
+        <main>
+          <Users
+            users={this.state.users}
+            onEdit={this.editUser}
+            onDelete={this.deleteUser}
+          />
+        </main>
+        <aside>
+          <AddUser onAdd={this.addUser} />
+        </aside>
+      </div>
+    );
+  }
+
+  deleteUser(id) {
+    this.setState({
+      users: this.state.users.filter((el) => el.id !== id),
+    });
+  }
+
+  editUser(user) {
+    let allUsers = this.state.users;
+    allUsers[user.id - 1] = user;
+    // console.log(user);
+
+    this.setState({ users: [] }, () => {
+      this.setState({ users: [...allUsers] });
+    });
+  }
+
+  addUser(user) {
+    const id = this.state.users.length + 1;
+    this.setState({ users: [...this.state.users, { id, ...user }] });
+    console.log(user);
+  }
+}
+
+export default App;
---
+++ src/components/User.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import AddUser from "./AddUser";
+import { IoCloseCircleSharp, IoHammerSharp } from "react-icons/io5";
+
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editForm: false,
+    };
+  }
+  user = this.props.user;
+  render() {
+    return (
+      <div className="user">
+        <IoCloseCircleSharp
+          onClick={() => this.props.onDelete(this.user.id)}
+          className="delete-icon"
+        />
+        <IoHammerSharp
+          onClick={() => {
+            this.setState({
+              editForm: !this.state.editForm,
+            });
+          }}
+          className="edit-icon"
+        />
+        <h3>
+          {this.user.first_name} {this.user.last_name}
+        </h3>
+        <img src={this.user.avatar} />
+        <p>{this.user.email}</p>
+        <b>{this.user.isHappy ? "Happy :)" : "not very happy :("}</b>
+
+        {this.state.editForm && (
+          <AddUser user={this.user} onAdd={this.props.onEdit} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default User;
